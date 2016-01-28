a1c0e8c96a60e3fe0fc7cad9849b542d9802a6ea
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-01-28 21:31:52 +0530

    Adding gen command
    with this now we can generate files using configurations on WASP

---
+++ cmd/gen.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+  "fmt"
+  "os"
+  "errors"
+  "text/template"
+
+  "github.com/spf13/cobra"
+  "github.com/ashwanthkumar/wasp-cli/util"
+)
+
+var GenCommand = &cobra.Command{
+  Use:   "gen <path> <tmpl_file>",
+  Short: "Generate go templates using WASP configurations",
+  Long:  `Generate go templates using WASP configurations`,
+  Run: AttachHandler(performGen),
+}
+
+func init() {
+  WaspCommand.AddCommand(GenCommand)
+}
+
+func performGen(args []string) (err error) {
+  if(len(args) != 2) {
+    return errors.New("gen takes exactly 2 arguments")
+  }
+
+  path := args[0]
+  tmplFile := args[1]
+  data, err := wasp.Get(path)
+  if err == nil {
+    var container interface{}
+    util.JsonDecode(data, &container)
+
+    // catch the template.Must's panic and return that as an error
+    defer func() {
+      if recovered := recover(); recovered != nil { //catch
+        err = errors.New(fmt.Sprintf("Exception: %v\n", recovered))
+      }
+    }()
+    return template.Must(template.ParseFiles(tmplFile)).Execute(os.Stdout, container)
+  }
+
+  return err
+}
